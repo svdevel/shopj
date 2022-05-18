1b8947d9c7199c314defa5b8076515e6afa7f8c2
svetlana.webdew <email@example.com>
2022-05-18 16:05:59 +0200

    Debug

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,8 +18,8 @@
 
 
   <!-- Styles -->
-  <link href="{{ asset('admin/css/moncss.css') }}" rel="stylesheet">
-  <link href="{{ asset('admin/css/material-dashboard.css') }}" rel="stylesheet">
+  <link href="admin/css/moncss.css" rel="stylesheet">
+  <link href="admin/css/material-dashboard.css" rel="stylesheet">
 </head>
 
 <body>
@@ -38,10 +38,10 @@
 
 
   <!--Script dashbord admin -->
-    <script src="{{ asset('admin/js/jquery.min.js') }}" defer></script>
-    <script src="{{ asset('admin/js/popper.min.js') }}" defer></script>
-    <script src="{{ asset('admin/js/bootstrap-material-design.min.js') }}" defer></script>
-    <script src="{{ asset('admin/js/perfect-scrollbar.jquery.min.js') }}" defer></script>
+    <script src="admin/js/jquery.min.js" defer></script>
+    <script src="admin/js/popper.min.js" defer></script>
+    <script src="admin/js/bootstrap-material-design.min.js" defer></script>
+    <script src="admin/js/perfect-scrollbar.jquery.min.js" defer></script>
 
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     @if (session('status'))
