4285def8044b5c3b80b946281c6ecd06cee35f5a
svetlana.webdew <email@example.com>
2023-01-24 15:49:35 +0100

    make it better

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,8 +9,6 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title')</title>
-<link rel="icon" href="{!! asset('images/Logo.png') !!}"/>
-...
 
   <link rel="stylesheet" type="text/css"
     href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
